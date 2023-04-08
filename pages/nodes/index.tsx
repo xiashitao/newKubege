58dacf1d4c81c9b13c5dff1074e8be57902c4f4b
wbxst01016635 <xst01016635@alibaba-inc.com>
2023-04-08 14:42:04 +0800

    feat:init

---
+++ pages/nodes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+function Nodes(props: any) {
+  return (
+    <div>
+      <Button type="primary"> Nodes</Button>
+    </div>
+  );
+}
+
+export default Nodes;
